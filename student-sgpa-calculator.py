9568b65b38a51e6e91a2a45fc06344aeb8b3c6c2
brcodes09 <brendan.rodrigues09@gmail.com>
2026-02-21 23:10:54 +0530

    Add files via upload

--- student-sgpa-calculator.py
+++ student-sgpa-calculator.py
@@ -1,15 +1,20 @@
 print("Welcome To Average Grade Pointer App")
-MDC = EG = PF = FESD = ECSE = IDT = AOC = MITT = 0
-UserMarks=[MDC,EG,PF,FESD,ECSE,IDT,AOC,MITT]
-MDC=int(input("Enter your MDC marks out of 150"))
-EG=int(input("Enter your EG marks out of 150"))
-PF=int(input("Enter your PF marks out of 200"))
-FESD=int(input("Enter your FESD marks out of 150"))
-ECSE=int(input("Enter your ECSE marks out of 100"))
-IDT=int(input("Enter your IDT marks out of 50"))
-AOC=int(input("Enter yout AOC marks out of 100"))
-MITT=int(input("Enter your MITT marks out of 100"))
 
+#Inputing marks per subject from user
+MDC=float(input("Enter your MDC marks out of 150:"))
+EG=float(input("Enter your EG marks out of 150:"))
+PF=float(input("Enter your PF marks out of 200:"))
+FESD=float(input("Enter your FESD marks out of 150:"))
+ECSE=float(input("Enter your ECSE marks out of 100:"))
+IDT=float(input("Enter your IDT marks out of 50:"))
+AOC=float(input("Enter yout AOC marks out of 100:"))
+MITT=float(input("Enter your MITT marks out of 100:"))
+
+UserMarks=[MDC,EG,PF,FESD,ECSE,IDT,AOC,MITT] #Adding it to a list if user needs only marks
+
+print(UserMarks) #Prints marks
+
+#Calculating percentage per  subject for Users marks
 MDCcent=(MDC/150)*100
 EGcent=(EG/150)*100
 PFcent=(PF/200)*100
@@ -19,44 +24,37 @@ IDTcent=(IDT/50)*100
 AOCcent=(AOC/100)*100
 MITTcent=(MITT/100)*100
 
-UserMarkscent=[MDCcent,EGcent,PFcent,FESDcent,ECSEcent,IDTcent,AOCcent,MITTcent]
+UserMarkscent=[MDCcent,EGcent,PFcent,FESDcent,ECSEcent,IDTcent,AOCcent,MITTcent]  #Adding it to a list if user needs only Percent
 
-MDCgrpointer,EGgrpointer,PFgrpointer,FESDgrpointer,ECSEgrpointer,IDTgrpointer,AOCgrpointer,MITTgrpointer=0
+print(UserMarkscent) #Prints percent per subject
 
-UserGrPointer=[MDCgrpointer,EGgrpointer,PFgrpointer,FESDgrpointer,ECSEgrpointer,IDTgrpointer,AOCgrpointer,MITTgrpointer]
-for items in UserMarkscent:
-    if items>=85:
-        for items in UserGrPointer:
-            if UserGrPointer[items]==UserMarkscent[items]:
-                UserGrPointer[items]=10
-    if 84>items and items>80:
-        for items in UserGrPointer:
-            if UserGrPointer[items]==UserMarkscent[items]:
-                UserGrPointer[items]=9
-    if 79>items and items>=70:
-        for items in UserGrPointer:
-            if UserGrPointer[items]==UserMarkscent[items]:
-                UserGrPointer[items]=8
-    if 69>items and items>=60:
-       for items in UserGrPointer:
-            if UserGrPointer[items]==UserMarkscent[items]:
-                UserGrPointer[items]=7
-    if 59>items and items>=50:
-        for items in UserGrPointer:
-            if UserGrPointer[items]==UserMarkscent[items]:
-                UserGrPointer[items]=6
-    if 49>items and items>=45:
-        for items in UserGrPointer:
-            if UserGrPointer[items]==UserMarkscent[items]:
-                UserGrPointer[items]=5
-    if 44>items and items>=40:
-        for items in UserGrPointer:
-            if UserGrPointer[items]==UserMarkscent[items]:
-                UserGrPointer[items]=4
-    if 40>items:
-        for items in UserGrPointer:
-            if UserGrPointer[items]==UserMarkscent[items]:
-                UserGrPointer[items]=0
+#Creating a list with 0's, no of 0's in the list is equal to the number of subjects
+UserGrPointer=[0,0,0,0,0,0,0,0] 
 
+#Accesing each index in the UserGrPointer list to assign equivalent Grade pointer
+for i in range(len(UserMarkscent)):
+    marks = UserMarkscent[i]
 
-Sgpa=(MDCgrpointer*3+EGgrpointer*3+PFgrpointer*4+FESDgrpointer*4+ECSEgrpointer*2+IDTgrpointer+AOCgrpointer*2+MITTgrpointer*2)/20
+    if marks >= 85:
+        UserGrPointer[i] = 10
+    elif marks >= 80:
+        UserGrPointer[i] = 9
+    elif marks >= 70:
+        UserGrPointer[i] = 8
+    elif marks >= 60:
+        UserGrPointer[i] = 7
+    elif marks >= 50:
+        UserGrPointer[i] = 6
+    elif marks >= 45:
+        UserGrPointer[i] = 5
+    elif marks >= 40:
+        UserGrPointer[i] = 4
+    else:
+        UserGrPointer[i] = 0
+
+print(UserGrPointer) #prints Grade pointer for each subject
+
+#Formula for calculating SGPA
+SGPA=(UserGrPointer[0]*3+UserGrPointer[1]*3+UserGrPointer[2]*4+UserGrPointer[3]*3+UserGrPointer[4]*2+UserGrPointer[5]*1+UserGrPointer[6]*2+UserGrPointer[7]*2)/20
+
+print(SGPA) #Prints SGPA
